19c1c74457c24f51b164174dcd7831f289dc0c37
Enzo Naguiat <enzo_naguiat@yahoo.com>
2018-08-22 15:39:20 +0800

    add tweets list, tweet, tweetbox components to the app and make them work properly

--- app/javascript/components/Main.js
+++ app/javascript/components/Main.js
@@ -3,19 +3,23 @@ import HelloWorld from "./HelloWorld"
 import TweetBox from "./TweetBox"
 import TweetList from "./TweetList"
 
-let mockTweets = [
-  {id: 1, author: "Enz", body: "first tweet"},
-  {id: 2, author: "Enz", body: "second tweet"},
-  {id: 3, author: "Enz", body: "third tweet"}
-];
+let mockTweets = [];
 
 export default class Main extends Component {
+  state = {tweets: mockTweets}
+
+  sendTweet = (tweet) => {
+    this.setState(prevState => ({
+      tweets: [ {id: 0, author: "guest", body: tweet}, ...prevState.tweets]
+    }))
+  }
+
   render () {
     return (
       <div className="container">
         <HelloWorld greeting="Hi pogi!" />
-        <TweetBox />
-        <TweetList tweets={mockTweets} />
+        <TweetBox sendTweet={this.sendTweet} />
+        <TweetList tweets={this.state.tweets} />
       </div>
     );
   }
--- app/javascript/components/TweetBox.js
+++ app/javascript/components/TweetBox.js
@@ -1,13 +1,26 @@
 import React from "react"
 
 export default class TweetBox extends React.Component {
+  state = {
+    tweet: ''
+  }
+
+  sendTweet = (event) => {
+    event.preventDefault();
+    this.props.sendTweet(this.state.tweet);
+    this.setState({tweet: ''});
+  }
+
   render () {
     return (
       <div>
-        <form>
+        <form onSubmit={this.sendTweet}>
           <div className="form-group">
             <label>Whats up?</label>
-            <textarea className="form-control form-control-lg"></textarea>
+            <textarea className="form-control form-control-lg"
+                      value={this.state.tweet}
+                      onChange={event => this.setState({tweet: event.target.value })}
+            ></textarea>
           </div>
           <button type='submit' className="btn btn-primary" style={{float: 'right'}}>Submit</button>
         </form>
