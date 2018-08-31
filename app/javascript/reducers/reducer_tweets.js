5ce84bac17f2b5dde70e8528cfd2b33b4b53686e
Enzo Naguiat <enzo_naguiat@yahoo.com>
2018-08-31 22:52:14 +0800

    modify tweets reducer to display sample data

--- app/javascript/reducers/reducer_tweets.js
+++ app/javascript/reducers/reducer_tweets.js
@@ -1,9 +1,7 @@
 export default function() {
-  // return [
-  //   {user_id: 1, body: "reduce this baby!"},
-  //   {user_id: 2, body: "redux yeah baby!"},
-  //   {user_id: 3, body: "reduuuuxxxx bo yeah baby!"}
-  // ]
-  return $.ajax('/tweets')
-    .done( data => this.setState({tweets:data}))
+  return [
+    {user_id: 1, body: "reduce this baby!"},
+    {user_id: 2, body: "redux yeah baby!"},
+    {user_id: 3, body: "reduuuuxxxx bo yeah baby!"}
+  ]
 };
