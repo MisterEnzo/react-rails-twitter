1f37bc648082514f012ea3b3aa9a9b378b88e9ce
Enzo Naguiat <enzo_naguiat@yahoo.com>
2018-09-15 17:41:33 +0800

    add gravatar styling

--- app/javascript/components/Tweet.js
+++ app/javascript/components/Tweet.js
@@ -6,10 +6,12 @@ export default class Tweet extends React.Component {
     function formattedDate(date){
       return moment(date).fromNow();
     }
+
     return (
       <li>
+        <img className="avatar" src={this.props.gravatar} />
         <span>{this.props.name}</span>
-        <p className='time'>{formattedDate(this.props.created_at)}</p>
+        <p className="time">{formattedDate(this.props.created_at)}</p>
         <p>{this.props.body}</p>
       </li>
     )
